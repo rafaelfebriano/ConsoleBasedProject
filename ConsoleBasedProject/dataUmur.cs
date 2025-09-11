2c3afcb8897b52b9f567f0ddd715dc16efa8a045
rafaelfebriano <febrianorafael54@gmail.com>
2025-09-11 17:03:03 +0700

    Add project files.

---
+++ ConsoleBasedProject/dataUmur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedProject
+{
+    internal class dataUmur
+    {
+
+        static void Main(string[]args)
+        {
+            Console.Write("Masukan Nama Anda:");
+            string nama = Console.ReadLine();
+
+            Console.Write("Masukan Umur Anda:");
+            int umur = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Halo " + nama + ", umur anda adalah " + umur + " tahun.");
+            Console.ReadKey();
+        }
+    }
+}
