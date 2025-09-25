ba5931f1cf0ee534a283b2431ec4931505e8424f
rafaelfebriano <febrianorafael54@gmail.com>
2025-09-25 17:08:21 +0700

    add meb 5

---
+++ ConsoleBasedProject/DaftarSiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedProject
+{
+    internal class DaftarSiswa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== DAFTAR NAMA SISWA ===\n");
+
+            
+            string[] siswa = { "Asep", "Budi", "Citra", "Dedi", "Eka" };
+
+            
+            foreach (string nama in siswa)
+            {
+                Console.WriteLine($"- {nama}");
+            }
+
+            Console.WriteLine("\nProgram selesai. Tekan ENTER untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}
